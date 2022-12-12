745488a2c3896de7a06493a88f94115ce4d439b7
sergio lopez <salo.tec.d@gmail.com>
2022-12-12 16:53:47 -0500

    Preentrega 2 finalizada

---
+++ canciones.js
@@ -0,0 +1,147 @@
+const artistas= [{
+
+    "id":1,
+    "artista":"Imagine Dragons",
+    "img":"./assets/enemy.jfif",
+    "genero":[
+        "rock",
+        "pop"
+    ],
+    "album":"Mercury-Act 1",
+    "cancion":"Enemy",
+    "fecha":"octubre 2021",
+    "precio":20000,
+    "oferta":true
+},
+{
+    "id":2,
+    "artista":"Imagine Dragons",
+    "img":"./assets/bones.jfif",
+    "genero":[
+        "rock",
+        "pop"
+    ],
+    "album":"Mercury-Act 2",
+    "cancion":"Bones",
+    "fecha":"Marzo 2022",
+    "precio":25000,
+    "oferta":false
+
+},
+{
+    "id":3,
+    "artista":"AJR",
+    "img":"./assets/bang.jfif",
+    "genero":[
+        "pop"
+    ],
+    "album":"N/A",
+    "cancion":"BANG!",
+    "fecha":"febrero 2020",
+    "precio":15000,
+    "oferta":true
+
+},
+{
+    "id":4,
+    "artista":"Panic At the Disco",
+    "img":"./assets/high_hopes.jfif",
+    "genero":[
+        "rock",
+        "pop"
+    ],
+    "album":"Pray for the Wicked",
+    "cancion":"High Hopes",
+    "fecha":"mayo 2018",
+    "precio":15000,
+    "oferta":true
+
+},
+{
+    "id":5,
+    "artista":"Walk The Moon",
+    "img":"./assets/shut_up_and_dance.jfif",
+    "genero":[
+        "rock"
+    ],
+    "album":"Talking is Hard",
+    "cancion":"Shut Up and Dance",
+    "fecha":"septiembre 2014",
+    "precio":10000,
+    "oferta":false
+
+},
+{
+    "id":6,
+    "artista":"Coldplay",
+    "img":"./assets/something_like_these.jfif",
+    "genero":[
+        "electronica",
+        "pop"
+    ],
+    "album":"Kaleidoscope",
+    "cancion":"Something like These",
+    "fecha":"junio 2017",
+    "precio":15000,
+    "oferta":false
+
+},
+{
+    "id":7,
+    "artista":"Billie Ellish",
+    "img":"./assets/bad_guy.jfif",
+    "genero":[
+        "electronica",
+        "pop"
+    ],
+    "album":"When we all fall asleep, where do we go?",
+    "cancion":"Bad Guy",
+    "fecha":"marzo 2019",
+    "precio":25000,
+    "oferta":true
+
+},
+{
+    "id":8,
+    "artista":"AJR",
+    "img":"./assets/sober_up.jfif",
+    "genero":[
+        "pop"
+    ],
+    "album":"The Click",
+    "cancion":"Sober UP",
+    "fecha":"marzo 2018",
+    "precio":20000,
+    "oferta":true
+
+},
+{
+    "id":9,
+    "artista":"Vigiland",
+    "img":"./assets/friday_night.jfif",
+    "genero":[
+        "electronica"
+    ],
+    "album":"Friday Night",
+    "cancion":"Friday Night",
+    "fecha":"marzo 2017",
+    "precio":15000,
+    "oferta":false
+
+},
+{
+    "id":10,
+    "artista":"One Republic",
+    "img":"./assets/Rescue_me.jfif",
+    "genero":[
+        "rock",
+        "pop"
+    ],
+    "album":"Human",
+    "cancion":"Rescue Me",
+    "fecha":"mayo 2019",
+    "precio":25000,
+    "oferta":false
+
+},
+]
---
+++ index.js
@@ -0,0 +1,249 @@
+function cardsAHtml (array){
+
+        const contenedor=document.querySelector(".contenedor")
+        document.querySelector(".contenedor").innerHTML=""
+        for (let i=0;i<array.length;i++){
+            
+            
+            const card=document.createElement("div")
+            card.className="card"
+            card.innerHTML=`
+                <div class="container-img">
+                    <img src=${array[i].img} alt=${array[i].name}
+                </div>
+                <h3>
+                    Artista:${array[i].artista}
+                </h3>
+                <h3>
+                    Genero:${array[i].genero}
+                </h3>
+                <h3>
+                    Album:${array[i].album}
+                </h3> 
+                <h3>
+                    Cancion:${array[i].cancion}
+                </h3> 
+                <h3>
+                    Fecha:${array[i].fecha}
+                </h3> 
+                <h3>
+                    Precio:$${array[i].precio}
+                </h3> 
+                                
+            `
+            contenedor.appendChild(card)
+            
+        }
+        
+    }
+
+function seleccionArtista(){
+    elemento=document.querySelector("#artistaSelect")
+    seleccion=elemento.value
+    return filtroArtista(seleccion)
+}
+function filtroArtista(seleccion){
+    
+    const filtro=artistas.filter((elemento)=>{
+        return elemento.artista===seleccion
+    })
+  
+
+    const contenedor=document.querySelector(".contenedor")
+    document.querySelector(".contenedor").innerHTML=""
+        for (let i=0;i<filtro.length;i++){
+            
+            
+            const card=document.createElement("div")
+            card.className="card"
+            card.innerHTML=`
+                <div class="container-img">
+                    <img src=${filtro[i].img} alt=${filtro[i].name}
+                </div>
+                <h3>
+                    Artista:${filtro[i].artista}
+                </h3>
+                <h3>
+                    Genero:${filtro[i].genero}
+                </h3>
+                <h3>
+                    Album:${filtro[i].album}
+                </h3> 
+                <h3>
+                    Cancion:${filtro[i].cancion}
+                </h3> 
+                <h3>
+                    Fecha:${filtro[i].fecha}
+                </h3> 
+                <h3>
+                    Precio:$${filtro[i].precio}
+                </h3> 
+                                
+            `
+            contenedor.appendChild(card)
+            
+        }
+}
+
+
+
+function ordenar(){
+    elemento=document.querySelector("#ordenAlfabetico")
+    seleccion=elemento.value
+    
+    if(seleccion==1){
+        return sortArtistaA(seleccion)
+    }
+    else{
+        return sortArtistaZ(seleccion)
+    }
+}
+
+function sortArtistaA(seleccion){
+    console.log(seleccion)
+
+   const artistasOrdenados=[...artistas].sort((a,b)=>{
+    if(a.artista<b.artista){
+        return -1
+    }
+    else if(a.artista>b.artista){
+        return 1
+    }
+    else{
+        return 0
+    }
+})
+    console.log(artistasOrdenados)
+    const contenedor=document.querySelector(".contenedor")
+    document.querySelector(".contenedor").innerHTML=""
+        for (let i=0;i<artistasOrdenados.length;i++){
+            
+            
+            const card=document.createElement("div")
+            card.className="card"
+            card.innerHTML=`
+                <div class="container-img">
+                    <img src=${artistasOrdenados[i].img} alt=${artistasOrdenados[i].name}
+                </div>
+                <h3>
+                    Artista:${artistasOrdenados[i].artista}
+                </h3>
+                <h3>
+                    Genero:${artistasOrdenados[i].genero}
+                </h3>
+                <h3>
+                    Album:${artistasOrdenados[i].album}
+                </h3> 
+                <h3>
+                    Cancion:${artistasOrdenados[i].cancion}
+                </h3> 
+                <h3>
+                    Fecha:${artistasOrdenados[i].fecha}
+                </h3> 
+                <h3>
+                    Precio:$${artistasOrdenados[i].precio}
+                </h3> 
+                                
+            `
+            contenedor.appendChild(card)
+            
+        }
+}
+
+function sortArtistaZ(seleccion){
+    console.log(seleccion)
+
+   const artistasOrdenados=[...artistas].sort((a,b)=>{
+    if(a.artista<b.artista){
+        return 1
+    }
+    else if(a.artista>b.artista){
+        return -1
+    }
+    else{
+        return 0
+    }
+})
+    console.log(artistasOrdenados)
+    const contenedor=document.querySelector(".contenedor")
+    document.querySelector(".contenedor").innerHTML=""
+    for (let i=0;i<artistasOrdenados.length;i++){
+            
+            
+        const card=document.createElement("div")
+        card.className="card"
+        card.innerHTML=`
+            <div class="container-img">
+                <img src=${artistasOrdenados[i].img} alt=${artistasOrdenados[i].name}
+            </div>
+            <h3>
+                Artista:${artistasOrdenados[i].artista}
+            </h3>
+            <h3>
+                Genero:${artistasOrdenados[i].genero}
+            </h3>
+            <h3>
+                Album:${artistasOrdenados[i].album}
+            </h3> 
+            <h3>
+                Cancion:${artistasOrdenados[i].cancion}
+            </h3> 
+            <h3>
+                Fecha:${artistasOrdenados[i].fecha}
+            </h3> 
+            <h3>
+                Precio:$${artistasOrdenados[i].precio}
+            </h3> 
+                            
+        `
+        contenedor.appendChild(card)
+        
+    }
+}
+
+
+function filtroOferta(){
+    
+    const filtro=artistas.filter((elemento)=>{
+        return elemento.oferta==true
+    })
+  
+
+    const contenedor=document.querySelector(".contenedor")
+    document.querySelector(".contenedor").innerHTML=""
+        for (let i=0;i<filtro.length;i++){
+            
+            
+            const card=document.createElement("div")
+            card.className="card"
+            card.innerHTML=`
+                <div class="container-img">
+                    <img src=${filtro[i].img} alt=${filtro[i].name}
+                </div>
+                <h3>
+                    Artista:${filtro[i].artista}
+                </h3>
+                <h3>
+                    Genero:${filtro[i].genero}
+                </h3>
+                <h3>
+                    Album:${filtro[i].album}
+                </h3> 
+                <h3>
+                    Cancion:${filtro[i].cancion}
+                </h3> 
+                <h3>
+                    Fecha:${filtro[i].fecha}
+                </h3> 
+                <h3>
+                    Precio:$${filtro[i].precio}
+                </h3>
+                <h3> 
+                Precio con descuento del 10%:$${filtro[i].precio-filtro[i].precio*10/100}
+                </h3>
+                                
+            `
+            contenedor.appendChild(card)
+            
+        }
+}
